9dd69782af95b0069b50848c8fa0b965ec7b11a1
zahramajd <zahra.majdabadi95@gmail.com>
2018-08-21 12:14:24 +0430

    initial

--- algorithms/lda/lda_scikit.py
+++ algorithms/lda/lda_scikit.py
@@ -1,2 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.decomposition import LatentDirichletAllocation
+
+
+def read_data(data_path):
+    df = pd.read_csv(data_path)
+    df.fillna(' ', inplace=True)
+    return df
+
+# Get the docs & make Bag of Words of them
+def create_bow(df):
+    docs =list( df.iloc[0:,0])
+
+    count_vectorizer = CountVectorizer()
+    docs_bag_of_words = count_vectorizer.fit_transform(docs)
+    docs_bag_of_words_feature_names = count_vectorizer.get_feature_names()
+    docs_bag_of_words_tfidf = TfidfTransformer().fit_transform(docs_bag_of_words)
+    
+    return docs_bag_of_words, docs_bag_of_words_feature_names, docs_bag_of_words_tfidf
+
+
 def lda_scikit(path):
-    return
+    df = read_data(data_path)
+
+    bow, bow_feature_names, bow_tfidf = create_bow(df)
+
+    lda = LatentDirichletAllocation(n_components =60, learning_method='batch').fit(bow)
+    topic_to_docs = lda.transform(bow)
+
+    return topic_to_docs
