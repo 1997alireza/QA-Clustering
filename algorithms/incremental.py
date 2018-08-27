42f2daa8a18651f0904439e7222456e4041a0f39
mahdi <mohammadmahdiabdollahpoor@gmail.com>
2018-08-28 01:33:51 +0430

    improvment in reducing clusters of len 1

--- algorithms/incremental.py
+++ algorithms/incremental.py
@@ -15,6 +15,8 @@ from org.apache.lucene.queryparser.classic import QueryParser
 from org.apache.lucene.analysis.core import StopAnalyzer
 from org.apache.lucene.analysis import StopwordAnalyzerBase
 from org.apache.lucene.analysis import StopFilter
+from org.apache.lucene.search.similarities import BM25Similarity
+
 import pandas as pd
 from java.nio.file import Paths
 from org.apache.lucene.store import SimpleFSDirectory
@@ -24,45 +26,46 @@ import numpy as np
 import random
 from org.apache.lucene.analysis.fa import PersianAnalyzer
 
-
-# from '../../tools' import load_stop_words
-# import '../../'tools as tols
+stop_words_address = 'incremental_stopwords.txt'
+# stop_words_address = '../persian-stopwords.txt'
 
 def load_stop_words():
-    stopwords_file = open('persian-stopwords.txt', 'r', encoding='utf8')
-    stopwords = stopwords_file.read().split('\n')
-    stopwords_file.close()
-    return stopwords
+    return [(x.strip()) for x in open(stop_words_address, 'r', encoding='utf8').read().split('\n')]
 
 
 lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 
-data = pd.ExcelFile('IrancellQA.xlsx')
-df1 = data.parse('preprocessed')
-df2 = data.parse('Raw')
-df1 = df1.applymap(str)
-df2 = df2.applymap(str)
-cols1 = df1.columns
-cols2 = df2.columns
+data = pd.ExcelFile('../IrancellQA.xlsx')
+preproc = data.parse('preprocessed')
+raw = data.parse('Raw')
+preproc = preproc.applymap(str)
+raw = raw.applymap(str)
+precols = preproc.columns
+rawcols = raw.columns
+soal = 0
+javab = 1
 
 
 def get_all_sentences():
     sentences = []
     for i in range(0, 14191):
         # for i in range(0, 1000):
-        rawA = df2[cols2[1]][i]
-        sentences.append(rawA)
+        preA = preproc[precols[javab]][i]
+        sentences.append(preA)
     return sentences
+
+
+sw = load_stop_words()
 
 
 class DocRepo:
     def __init__(self, indexes=None):
 
         # self.analyzer = StandardAnalyzer()
-        sw = load_stop_words()
 
         # self.analyzer = PersianAnalyzer(StopFilter.makeStopSet(sw))
-        self.analyzer = StopAnalyzer(Paths.get('persian-stopwords.txt'))
+        # self.analyzer = PersianAnalyzer()
+        self.analyzer = StopAnalyzer(Paths.get(stop_words_address))
         self.config = IndexWriterConfig(self.analyzer)
         self.index = RAMDirectory()
         if indexes is not None:
@@ -70,40 +73,21 @@ class DocRepo:
             print("is not none")
         else:
             self.loadIndex()
-        # print("hiiiiiii")
 
     def loadIndex(self, indexes=None):
-        # print("loading")
         self.w = IndexWriter(self.index, self.config)
         if indexes is None:
             for i in range(0, 14191):
-                # rawQ = df1[cols1[0]][i]
-                # rawA = df2[cols2[0]][i]
-                # preQ = df1[cols1[1]][i]
-                # self.addDoc(i,rawQ, preQ, rawA)
                 self.addDocument(i)
         else:
             for i in indexes:
-                # rawQ = df1[cols1[0]][i]
-                # rawA = df2[cols2[0]][i]
-                # preQ = df1[cols1[1]][i]
-                # self.addDoc(i,rawQ, preQ, rawA)
                 self.addDocument(i)
 
-    def addDoc(self, id, rawQuestion, preprocessedQuestion, RawAnswer):
-        doc = Document()
-        doc.add(TextField("rq", rawQuestion, Field.Store.YES))
-        doc.add(TextField("pq", preprocessedQuestion, Field.Store.YES))
-        doc.add(TextField("ra", RawAnswer, Field.Store.YES))
-        doc.add(StringField("id", str(id), Field.Store.YES))
-        self.w.addDocument(doc)
-        self.w.commit()
-
     def addDocument(self, id):
-        rawQ = df1[cols1[0]][id]
-        rawA = df2[cols2[0]][id]
-        preA = df2[cols2[1]][id]
-        preQ = df1[cols1[1]][id]
+        preQ = preproc[precols[soal]][id]
+        rawQ = raw[rawcols[soal]][id]
+        rawA = raw[rawcols[javab]][id]
+        preA = preproc[precols[javab]][id]
         doc = Document()
         doc.add(TextField("rq", rawQ, Field.Store.YES))
         doc.add(TextField("pq", preQ, Field.Store.YES))
@@ -117,85 +101,91 @@ class DocRepo:
         self.w.close()
         # print("died")
 
-    def get_most_similar(self, string):
+    def get_most_similar(self, sentence):
         # print('query string is',string)
-        # q = QueryParser(f = 'ra', a = self.analyzer).parse(string)
+        # q = QueryParser('pa', self.analyzer).parse(sentence)
         query_builder = BooleanQuery.Builder()
-        for q_word in string.split(' '):
-            qtq = TermQuery(Term("pa", q_word))
-            query_builder.add(BooleanClause(qtq, BooleanClause.Occur.SHOULD))
+        for token in sentence.split(' '):
+            if token not in sw:
+                qtq = TermQuery(Term("pa", token))
+                query_builder.add(BooleanClause(qtq, BooleanClause.Occur.SHOULD))
         q = query_builder.build()
         hitsPerPage = 2
         if self.index is None:
             print("its none dude")
+            return
         reader = DirectoryReader.open(self.w)
-        searcher = IndexSearcher(reader)
+        self.searcher = IndexSearcher(reader)
+        simi = BM25Similarity(12.0, 1.0)
+        self.searcher.setSimilarity(simi)
 
-        docs = searcher.search(q, hitsPerPage)
+        docs = self.searcher.search(q, hitsPerPage)
         hits = docs.scoreDocs
 
         # print("Found " + str(len(hits)) + " hits.")
         if len(hits) > 0:
-            mate = searcher.doc(hits[0].doc).get("id")
+            mate = self.searcher.doc(hits[0].doc).get("id")
             print("found something. mate: ", mate, "- score : ", hits[0].score)
+            reader.close()
             return hits[0], int(mate)
         else:
+            reader.close()
             return None, -1
 
-        reader.close()
+
+sentences = get_all_sentences()
 
 
 def do_cluster(path, threshold):
     clusters = []
     # repo = DocRepo(path)
-    sentences = get_all_sentences()
+
     random.shuffle(sentences)
     flags = []
     for i in range(0, len(sentences)):
         flags.append(-1)
     repo = DocRepo([])
+    # reader = DirectoryReader.open(repo.w)
+    # searcher = IndexSearcher(reader)
 
     print('number of sentences ', len(sentences))
     for senidx, sentence in enumerate(sentences):
-        best_macthing_cluster = -1
+        best_matching_cluster = -1
         # best_macthing_score = -1
         closest, mate = repo.get_most_similar(sentence)
         if (closest is not None) and (closest.score >= threshold):
-            #    cluss = [cl.index(mate) for cl in clusters if mate in cl]
-            #    if len(cluss) > 0:
-            #         best_macthing_cluster = cluss[0]
-            #         if len(cluss) > 1:
-            #             print("ajibbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
-            #             return
-            best_macthing_cluster = flags[mate]
-        if best_macthing_cluster == -1:
+            best_matching_cluster = flags[mate]
+        if best_matching_cluster == -1:
             clusters.append([])
             clusters[-1].append(senidx)
             print(senidx, ' creates new cluster')
             flags[senidx] = len(clusters) - 1
         else:
-            print(senidx, ' goes to cluster ', best_macthing_cluster)
-            clusters[best_macthing_cluster].append(senidx)
-            flags[senidx] = best_macthing_cluster
+            print(senidx, ' goes to cluster ', best_matching_cluster)
+            clusters[best_matching_cluster].append(senidx)
+            flags[senidx] = best_matching_cluster
         repo.addDocument(senidx)
-    return clusters
+    return clusters, repo
 
 
-res = do_cluster('IrancellQA.xlsx', threshold=1)
+# repo = DocRepo()
 
+#
+# print(df2[cols2[1]][0])
+# print((searcher.doc(0)).get('pa'))
+
+res, repo = do_cluster('IrancellQA.xlsx', threshold=10)
 print([len(cluster) for cluster in res])
 print(len(res))
-cnt = 0
-for cl in res:
-    if len(cl) == 1:
-        print(df2[cols2[1]][cl[0]])
-        print("\n")
+ones = [cl for cl in res if len(cl) == 1]
+print('ones : ', len(ones))
+with open('ones.txt', 'w', encoding='utf-8') as f:
+    for one in ones:
+        f.write(str(sentences[one[0]]))
+        f.write("\n")
+        # f.write("----------------------------------------")
 
-        cnt += 1
-print(cnt)
-
-# analyze = PersianAnalyzer()
+# analyze = o()
 # print(analyze.getDefaultStopSet())
 # sw  = load_stop_words()
 # print(sw)
-
